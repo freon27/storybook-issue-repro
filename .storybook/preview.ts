a3de1aa0c9077fb5929aaef3677a580f691c3640
Nick Peters <nicholas.peters@akelius.de>
2023-03-03 16:41:00 +0100

    first commit

---
+++ .storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from '@storybook/angular';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
+setCompodocJson(docJson);
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/stories/Header.stories.ts
@@ -0,0 +1,47 @@
+import { moduleMetadata } from '@storybook/angular';
+import type { Meta, StoryObj } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+
+import Button from './button.component';
+import Header from './header.component';
+
+
+const meta: Meta<Header> = {
+  title: 'Example/Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/angular/writing-docs/docs-page
+  tags: ['autodocs'],
+  render: (args) => ({ props: args }),
+  decorators: [
+    moduleMetadata({
+      declarations: [Button],
+      imports: [CommonModule],
+    }),
+  ],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/7.0/angular/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+/**
+ * Description could go here 1
+ */
+export default meta;
+type Story = StoryObj<Header>;
+
+/**
+ * Description could go here 2
+ */
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+/**
+ * Description could go here 3
+ */
+export const LoggedOut: Story = {};
